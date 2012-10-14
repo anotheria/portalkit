ef92fbfec9c43e028f4ca8baacf08c9ab2d53174
lrosenberg <lrosenberg@a8275d70-9d91-4e1b-b379-151a6989f1a2>
2012-10-14 20:00:06 +0000

    still importing structure

---
+++ services/accountservice/java/net/anotheria/portalkit/services/account/AccountService.java
@@ -0,0 +1,10 @@
+package net.anotheria.portalkit.services.account;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 14.10.12 21:56
+ */
+public interface AccountService {
+}
---
+++ services/common/java/net/anotheria/portalkit/services/common/AccountId.java
@@ -0,0 +1,10 @@
+package net.anotheria.portalkit.services.common;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 14.10.12 21:54
+ */
+public class AccountId {
+}
