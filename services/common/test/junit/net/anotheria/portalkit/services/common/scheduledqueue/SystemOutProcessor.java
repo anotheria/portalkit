8a51b926d354c32e33593d3a693d499e6163a6a6
abolbat <abolbat@a8275d70-9d91-4e1b-b379-151a6989f1a2>
2012-11-22 16:10:22 +0000

    ScheduledQueue JUnit improvement

--- services/common/test/junit/net/anotheria/portalkit/services/common/scheduledqueue/SystemOutProcessor.java
+++ services/common/test/junit/net/anotheria/portalkit/services/common/scheduledqueue/SystemOutProcessor.java
@@ -1,5 +1,9 @@
 package net.anotheria.portalkit.services.common.scheduledqueue;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.log4j.Logger;
+
 /**
  * {@link Processor} testing implementation.
  * 
@@ -8,18 +12,23 @@ package net.anotheria.portalkit.services.common.scheduledqueue;
 public class SystemOutProcessor implements Processor {
 
 	/**
+	 * {@link Logger} instance.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(SystemOutProcessor.class);
+
+	/**
 	 * Processed elements amount.
 	 */
-	private int processed;
+	private static final AtomicInteger processed = new AtomicInteger(0);
 
 	@Override
 	public void process(final Object element) throws ProcessingException {
-		System.out.println(System.currentTimeMillis() + " element: " + element);
-		processed++;
+		LOGGER.info("Element[" + element + "], processed at: " + System.currentTimeMillis());
+		processed.incrementAndGet();
 	}
 
 	public int getProcessed() {
-		return processed;
+		return processed.get();
 	}
 
 }
