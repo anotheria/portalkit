554bfa1a221dc7a3abe189fe2a2915c6203d1311
dagafonov <dagafonov@a8275d70-9d91-4e1b-b379-151a6989f1a2>
2013-01-22 13:49:56 +0000

    added caches in ForeignIdService

--- services/foreignidservice/java/net/anotheria/portalkit/services/foreignid/ForeignId.java
+++ services/foreignidservice/java/net/anotheria/portalkit/services/foreignid/ForeignId.java
@@ -27,14 +27,14 @@ public class ForeignId implements Serializable {
 	 * The account id in our system
 	 */
 	private AccountId accountId;
-	
+
 	/**
 	 * Default constructor
 	 */
 	public ForeignId() {
-		
+
 	}
-	
+
 	public ForeignId(AccountId accountId, int sourceId, String id) {
 		this.accountId = accountId;
 		this.sourceId = sourceId;
@@ -69,7 +69,6 @@ public class ForeignId implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((accountId == null) ? 0 : accountId.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + sourceId;
 		return result;
@@ -84,11 +83,6 @@ public class ForeignId implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		ForeignId other = (ForeignId) obj;
-		if (accountId == null) {
-			if (other.accountId != null)
-				return false;
-		} else if (!accountId.equals(other.accountId))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
--- services/foreignidservice/java/net/anotheria/portalkit/services/foreignid/ForeignIdServiceImpl.java
+++ services/foreignidservice/java/net/anotheria/portalkit/services/foreignid/ForeignIdServiceImpl.java
@@ -1,18 +1,46 @@
 package net.anotheria.portalkit.services.foreignid;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import net.anotheria.anoprise.cache.Cache;
+import net.anotheria.anoprise.cache.Caches;
 import net.anotheria.anoprise.metafactory.MetaFactory;
 import net.anotheria.anoprise.metafactory.MetaFactoryException;
 import net.anotheria.portalkit.services.common.AccountId;
 import net.anotheria.portalkit.services.foreignid.persistence.ForeignIdPersistenceService;
 import net.anotheria.portalkit.services.foreignid.persistence.ForeignIdPersistenceServiceException;
 
+/**
+ * ForeignId Service implementation.
+ * 
+ * @author dagafonov
+ */
 public class ForeignIdServiceImpl implements ForeignIdService {
 
+	/**
+	 * Persistence service.
+	 */
 	private ForeignIdPersistenceService persistenceService;
 
+	/**
+	 * Cache by {@link AccountId}.
+	 */
+	private Cache<AccountId, List<ForeignId>> cacheByAccountId;
+
+	/**
+	 * Cache by string representation of all foreign id fields.
+	 */
+	private Cache<String, AccountId> cacheByForeignId;
+
+	/**
+	 * Default Constructor.
+	 */
 	public ForeignIdServiceImpl() {
+
+		cacheByAccountId = Caches.createHardwiredCache("foreignidservice-cacheaccountid");
+		cacheByForeignId = Caches.createHardwiredCache("foreignidservice-cacheforeignid");
+
 		try {
 			persistenceService = MetaFactory.get(ForeignIdPersistenceService.class);
 		} catch (MetaFactoryException e) {
@@ -20,10 +48,28 @@ public class ForeignIdServiceImpl implements ForeignIdService {
 		}
 	}
 
+	/**
+	 * Returns String representation of all foreign id fields.
+	 * 
+	 * @param foreignId
+	 * @param sourceId
+	 * @return
+	 */
+	private String getForeignIdString(String foreignId, int sourceId) {
+		return "fid_" + sourceId + "_" + foreignId;
+	}
+
 	@Override
 	public void addForeignId(AccountId accId, String foreignId, int sourceId) throws ForeignIdServiceException {
 		try {
 			persistenceService.link(accId, sourceId, foreignId);
+			List<ForeignId> list = cacheByAccountId.get(accId);
+			if (list == null) {
+				list = new ArrayList<ForeignId>();
+				cacheByAccountId.put(accId, list);
+			}
+			list.add(new ForeignId(accId, sourceId, foreignId));
+			cacheByForeignId.put(getForeignIdString(foreignId, sourceId), accId);
 		} catch (ForeignIdPersistenceServiceException e) {
 			throw new ForeignIdServiceException("persistenceService.link failed", e);
 		}
@@ -33,6 +79,9 @@ public class ForeignIdServiceImpl implements ForeignIdService {
 	public void removeForeignId(AccountId accId, String foreignId, int sourceId) throws ForeignIdServiceException {
 		try {
 			persistenceService.unlink(accId, sourceId, foreignId);
+			List<ForeignId> list = cacheByAccountId.get(accId);
+			list.remove(new ForeignId(accId, sourceId, foreignId));
+			cacheByForeignId.remove(getForeignIdString(foreignId, sourceId));
 		} catch (ForeignIdPersistenceServiceException e) {
 			throw new ForeignIdServiceException("persistenceService.unlink failed", e);
 		}
@@ -40,17 +89,40 @@ public class ForeignIdServiceImpl implements ForeignIdService {
 
 	@Override
 	public AccountId getAccountIdByForeignId(String foreignId, int sourceId) throws ForeignIdServiceException {
+		if (foreignId == null) {
+			throw new IllegalArgumentException("Null parameter foreignId to getForeignIds(foreignId, sourceId)");
+		}
+		AccountId accId = cacheByForeignId.get(getForeignIdString(foreignId, sourceId));
+		if (accId != null) {
+			return accId;
+		}
 		try {
-			return persistenceService.getAccountIdByForeignId(sourceId, foreignId);
+			AccountId fromPersistence = persistenceService.getAccountIdByForeignId(sourceId, foreignId);
+			if (fromPersistence != null) {
+				cacheByForeignId.put(getForeignIdString(foreignId, sourceId), fromPersistence);
+			}
+			return fromPersistence;
 		} catch (ForeignIdPersistenceServiceException e) {
 			throw new ForeignIdServiceException("persistenceService.getAccountIdByForeignId failed", e);
 		}
 	}
 
 	@Override
-	public List<ForeignId> getForeignIds(AccountId accountId) throws ForeignIdServiceException {
+	public List<ForeignId> getForeignIds(AccountId accId) throws ForeignIdServiceException {
+		if (accId == null) {
+			throw new IllegalArgumentException("Null parameter accId to getForeignIds(accId)");
+		}
+		List<ForeignId> fromCache = cacheByAccountId.get(accId);
+		if (fromCache != null) {
+			return fromCache;
+		}
 		try {
-			return persistenceService.getForeignIdsByAccountId(accountId);
+			List<ForeignId> fromPersistence = persistenceService.getForeignIdsByAccountId(accId);
+			if (fromPersistence == null) {
+				fromPersistence = new ArrayList<ForeignId>();
+			}
+			cacheByAccountId.put(accId, fromPersistence);
+			return fromPersistence;
 		} catch (ForeignIdPersistenceServiceException e) {
 			throw new ForeignIdServiceException("persistenceService.getForeignIdsByAccountId failed", e);
 		}
