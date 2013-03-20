b6c7e615806b1c3b930e78706f3684b84a6f47db
rsuharnik <rsuharnik@a8275d70-9d91-4e1b-b379-151a6989f1a2>
2013-03-20 18:24:56 +0000

    todo  added

--- services/accountlistservice/java/net/anotheria/portalkit/services/accountlist/AccountListServiceImpl.java
+++ services/accountlistservice/java/net/anotheria/portalkit/services/accountlist/AccountListServiceImpl.java
@@ -24,6 +24,9 @@ import org.apache.log4j.Logger;
  * AccountList service implementation.
  *
  * @author dagafonov
+ *
+ * //TODO :  curent implementation does not provides update functionality...  It's  important for ( visitS - last  visit time update  for  example) Seems  we need to remove  create, and  add save  method!
+ * //TODO : last  changes  were done - to prevent Unique key violation errors - which was possible .. Added  as Temp  solution!  - but  when SAVE  will be provided - last  changes should be simply rolled back!
  */
 public class AccountListServiceImpl implements AccountListService {
     /**
