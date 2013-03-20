8dff59542a54ff972a3d5d77aae0870dddfbfe13
rsuharnik <rsuharnik@a8275d70-9d91-4e1b-b379-151a6989f1a2>
2013-03-20 16:55:56 +0000

    initial commit.

--- services/accountlistservice/java/net/anotheria/portalkit/services/accountlist/persistence/jdbc/AccountListDAO.java
+++ services/accountlistservice/java/net/anotheria/portalkit/services/accountlist/persistence/jdbc/AccountListDAO.java
@@ -17,159 +17,189 @@ import net.anotheria.portalkit.services.common.persistence.jdbc.JDBCUtil;
 
 /**
  * Account list DAO implementation.
- * 
+ *
  * @author dagafonov
- * 
  */
 public class AccountListDAO extends AbstractDAO implements DAO {
 
-	private static final String TABLE_NAME = "accountlist";
-	private static final String OWNER_ID = "owner";
-	private static final String TARGET_ID = "target";
-	private static final String LIST_NAME = "listName";
-	private static final String ADDITIONAL_INFO = "additionalinfo";
-	private static final String CREATION_TIMESTAMP = "creationTimestamp";
+    private static final String TABLE_NAME = "accountlist";
+    private static final String OWNER_ID = "owner";
+    private static final String TARGET_ID = "target";
+    private static final String LIST_NAME = "listName";
+    private static final String ADDITIONAL_INFO = "additionalinfo";
+    private static final String CREATION_TIMESTAMP = "creationTimestamp";
 
-	@Override
-	protected String[] getTableNames() {
-		return new String[] { TABLE_NAME };
-	}
+    @Override
+    protected String[] getTableNames() {
+        return new String[]{TABLE_NAME};
+    }
 
-	@Override
-	public void cleanupFromUnitTests(Connection connection) throws DAOException, SQLException {
-		super.cleanupFromUnitTests(connection);
-	}
+    @Override
+    public void cleanupFromUnitTests(Connection connection) throws DAOException, SQLException {
+        super.cleanupFromUnitTests(connection);
+    }
 
-	/**
-	 * Gets list of accounts that belongs to specified account ant account list.
-	 * Never returs null value.
-	 * 
-	 * @param connection
-	 *            current connection to DB.
-	 * @param owner
-	 * @param listName
-	 * @return
-	 * @throws DAOException
-	 * @throws SQLException
-	 */
-	public List<AccountIdAdditionalInfo> getList(Connection connection, AccountId owner, String listName) throws DAOException, SQLException {
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
+    /**
+     * Gets list of accounts that belongs to specified account ant account list.
+     * Never returs null value.
+     *
+     * @param connection current connection to DB.
+     * @param owner
+     * @param listName
+     * @return
+     * @throws DAOException
+     * @throws SQLException
+     */
+    public List<AccountIdAdditionalInfo> getList(Connection connection, AccountId owner, String listName) throws DAOException, SQLException {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
 
-			List<AccountIdAdditionalInfo> res = new ArrayList<AccountIdAdditionalInfo>();
-			stmt = connection.prepareStatement(String.format("SELECT %s, %s, %s from %s WHERE %s=? and %s=? order by %s asc", TARGET_ID, ADDITIONAL_INFO,
-					CREATION_TIMESTAMP, TABLE_NAME, OWNER_ID, LIST_NAME, CREATION_TIMESTAMP));
-			stmt.setString(1, owner.getInternalId());
-			stmt.setString(2, listName);
-			rs = stmt.executeQuery();
-			while (rs.next()) {
-				AccountIdAdditionalInfo aidai = new AccountIdAdditionalInfo(new AccountId(rs.getString(TARGET_ID)), rs.getString(ADDITIONAL_INFO));
-				aidai.setCreationTimestamp(rs.getLong(CREATION_TIMESTAMP));
-				res.add(aidai);
-			}
-			return res;
-		} finally {
-			JDBCUtil.close(rs);
-			JDBCUtil.close(stmt);
-		}
-	}
+            List<AccountIdAdditionalInfo> res = new ArrayList<AccountIdAdditionalInfo>();
+            stmt = connection.prepareStatement(String.format("SELECT %s, %s, %s from %s WHERE %s=? and %s=? order by %s asc", TARGET_ID, ADDITIONAL_INFO,
+                    CREATION_TIMESTAMP, TABLE_NAME, OWNER_ID, LIST_NAME, CREATION_TIMESTAMP));
+            stmt.setString(1, owner.getInternalId());
+            stmt.setString(2, listName);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                AccountIdAdditionalInfo aidai = new AccountIdAdditionalInfo(new AccountId(rs.getString(TARGET_ID)), rs.getString(ADDITIONAL_INFO));
+                aidai.setCreationTimestamp(rs.getLong(CREATION_TIMESTAMP));
+                res.add(aidai);
+            }
+            return res;
+        } finally {
+            JDBCUtil.close(rs);
+            JDBCUtil.close(stmt);
+        }
+    }
 
-	/**
-	 * Adds targets into specified list of accounts for specified account.
-	 * 
-	 * @param connection
-	 *            current connection to DB.
-	 * @param owner
-	 * @param listName
-	 * @param targets
-	 * @return
-	 * @throws DAOException
-	 * @throws SQLException
-	 */
-	public boolean addToList(Connection connection, AccountId owner, String listName, Collection<AccountIdAdditionalInfo> targets)
-			throws DAOException, SQLException {
-		PreparedStatement insertStmt = null;
-		try {
-			String prefSQL = String.format("insert into %s (%s, %s, %s, %s, %s) values (?, ?, ?, ?, ?)", TABLE_NAME, OWNER_ID, TARGET_ID, LIST_NAME,
-					ADDITIONAL_INFO, CREATION_TIMESTAMP);
-			insertStmt = connection.prepareStatement(prefSQL);
-			for (AccountIdAdditionalInfo accId : targets) {
-				insertStmt.setString(1, owner.getInternalId());
-				insertStmt.setString(2, accId.getAccountId().getInternalId());
-				insertStmt.setString(3, listName);
-				insertStmt.setString(4, accId.getAdditionalInfo());
-				insertStmt.setLong(5, accId.getCreationTimestamp());
-				insertStmt.addBatch();
-			}
-			insertStmt.executeBatch();
-			return true;
-		} finally {
-			JDBCUtil.close(insertStmt);
-		}
-	}
+    /**
+     * Adds targets into specified list of accounts for specified account.
+     *
+     * @param connection current connection to DB.
+     * @param owner
+     * @param listName
+     * @param targets
+     * @return
+     * @throws DAOException
+     * @throws SQLException
+     */
+    public boolean addToList(Connection connection, AccountId owner, String listName, Collection<AccountIdAdditionalInfo> targets)
+            throws DAOException, SQLException {
+        PreparedStatement insertStmt = null;
+        try {
+            String prefSQL = String.format("insert into %s (%s, %s, %s, %s, %s) values (?, ?, ?, ?, ?)", TABLE_NAME, OWNER_ID, TARGET_ID, LIST_NAME,
+                    ADDITIONAL_INFO, CREATION_TIMESTAMP);
 
-	/**
-	 * Removes targets from specified list of accounts for specified account.
-	 * 
-	 * @param connection
-	 *            current connection to DB.
-	 * @param owner
-	 * @param listName
-	 * @param targets
-	 * @return
-	 * @throws DAOException
-	 * @throws SQLException
-	 */
-	public boolean removeFromList(Connection connection, AccountId owner, String listName, Collection<AccountIdAdditionalInfo> targets)
-			throws DAOException, SQLException {
-		PreparedStatement deleteStmt = null;
-		try {
-			String prefSQL = String.format("delete from %s where %s=? and %s=? and %s=? ;", TABLE_NAME, OWNER_ID, TARGET_ID, LIST_NAME);
-			deleteStmt = connection.prepareStatement(prefSQL);
-			for (AccountIdAdditionalInfo accId : targets) {
-				deleteStmt.setString(1, owner.getInternalId());
-				deleteStmt.setString(2, accId.getAccountId().getInternalId());
-				deleteStmt.setString(3, listName);
-				deleteStmt.addBatch();
-			}
-			deleteStmt.executeBatch();
-			return true;
-		} finally {
-			JDBCUtil.close(deleteStmt);
-		}
-	}
+            connection.setAutoCommit(false);
+            insertStmt = connection.prepareStatement(prefSQL);
 
-	/**
-	 * Gets list of accounts where I am in the specified list of specified
-	 * account.
-	 * 
-	 * @param connection
-	 * @param target
-	 * @param listName
-	 * @return
-	 * @throws DAOException
-	 * @throws SQLException
-	 */
-	public List<AccountIdAdditionalInfo> getReverseList(Connection connection, AccountId target, String listName) throws DAOException, SQLException {
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
-			List<AccountIdAdditionalInfo> res = new ArrayList<AccountIdAdditionalInfo>();
-			stmt = connection.prepareStatement(String.format("SELECT %s, %s, %s from %s WHERE %s=? and %s=? order by %s asc", OWNER_ID, ADDITIONAL_INFO, CREATION_TIMESTAMP, TABLE_NAME,
-					TARGET_ID, LIST_NAME, CREATION_TIMESTAMP));
-			stmt.setString(1, target.getInternalId());
-			stmt.setString(2, listName);
-			rs = stmt.executeQuery();
-			while (rs.next()) {
-				AccountIdAdditionalInfo aidai = new AccountIdAdditionalInfo(new AccountId(rs.getString(OWNER_ID)), rs.getString(ADDITIONAL_INFO));
-				aidai.setCreationTimestamp(rs.getLong(CREATION_TIMESTAMP));
-				res.add(aidai);
-			}
-			return res;
-		} finally {
-			JDBCUtil.close(rs);
-			JDBCUtil.close(stmt);
-		}
-	}
+            final boolean batchSupport = connection.getMetaData().supportsBatchUpdates();
+
+            for (AccountIdAdditionalInfo accId : targets) {
+                //in case when batch is not supported - should clear prev  params
+                if (!batchSupport)
+                    insertStmt.clearParameters();
+
+                insertStmt.setString(1, owner.getInternalId());
+                insertStmt.setString(2, accId.getAccountId().getInternalId());
+                insertStmt.setString(3, listName);
+                insertStmt.setString(4, accId.getAdditionalInfo());
+                insertStmt.setLong(5, accId.getCreationTimestamp());
+
+                // performing update - or add to batch
+                if (!batchSupport)
+                    insertStmt.executeUpdate();
+                else
+                    insertStmt.addBatch();
+            }
+            //execute batch if possible
+            if (batchSupport)
+                insertStmt.executeBatch();
+
+            //committing stuff
+            connection.commit();
+            return true;
+        } finally {
+            JDBCUtil.close(insertStmt);
+        }
+    }
+
+
+    /**
+     * Removes targets from specified list of accounts for specified account.
+     *
+     * @param connection current connection to DB.
+     * @param owner
+     * @param listName
+     * @param targets
+     * @return
+     * @throws DAOException
+     * @throws SQLException
+     */
+    public boolean removeFromList(Connection connection, AccountId owner, String listName, Collection<AccountIdAdditionalInfo> targets)
+            throws DAOException, SQLException {
+        PreparedStatement deleteStmt = null;
+        try {
+            String prefSQL = String.format("delete from %s where %s=? and %s=? and %s=? ;", TABLE_NAME, OWNER_ID, TARGET_ID, LIST_NAME);
+            final boolean batchSupport = connection.getMetaData().supportsBatchUpdates();
+            connection.setAutoCommit(false);
+            deleteStmt = connection.prepareStatement(prefSQL);
+
+            for (AccountIdAdditionalInfo accId : targets) {
+                //clear prev stuff - when no natch is supported
+                if (!batchSupport)
+                    deleteStmt.clearParameters();
+
+                deleteStmt.setString(1, owner.getInternalId());
+                deleteStmt.setString(2, accId.getAccountId().getInternalId());
+                deleteStmt.setString(3, listName);
+                //depending on batch support - add to the batch or  simply execute immediately
+                if (!batchSupport)
+                    deleteStmt.executeUpdate();
+                else
+                    deleteStmt.addBatch();
+            }
+            if (batchSupport)
+                deleteStmt.executeBatch();
+
+            connection.commit();
+            return true;
+        } finally {
+            JDBCUtil.close(deleteStmt);
+        }
+    }
+
+    /**
+     * Gets list of accounts where I am in the specified list of specified
+     * account.
+     *
+     * @param connection
+     * @param target
+     * @param listName
+     * @return
+     * @throws DAOException
+     * @throws SQLException
+     */
+    public List<AccountIdAdditionalInfo> getReverseList(Connection connection, AccountId target, String listName) throws DAOException, SQLException {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            List<AccountIdAdditionalInfo> res = new ArrayList<AccountIdAdditionalInfo>();
+            stmt = connection.prepareStatement(String.format("SELECT %s, %s, %s from %s WHERE %s=? and %s=? order by %s asc", OWNER_ID, ADDITIONAL_INFO, CREATION_TIMESTAMP, TABLE_NAME,
+                    TARGET_ID, LIST_NAME, CREATION_TIMESTAMP));
+            stmt.setString(1, target.getInternalId());
+            stmt.setString(2, listName);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                AccountIdAdditionalInfo aidai = new AccountIdAdditionalInfo(new AccountId(rs.getString(OWNER_ID)), rs.getString(ADDITIONAL_INFO));
+                aidai.setCreationTimestamp(rs.getLong(CREATION_TIMESTAMP));
+                res.add(aidai);
+            }
+            return res;
+        } finally {
+            JDBCUtil.close(rs);
+            JDBCUtil.close(stmt);
+        }
+    }
 }
