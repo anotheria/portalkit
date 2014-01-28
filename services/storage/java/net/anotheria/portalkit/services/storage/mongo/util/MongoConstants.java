9c1c5cb18b2ebfe12b3ffcc4d006a51ebb6f1186
Ivan Batura <Ivan.Batura@gmail.com>
2014-01-28 15:08:21 +0200

    - add mongo constants for operations

--- services/storage/java/net/anotheria/portalkit/services/storage/mongo/util/MongoConstants.java
+++ services/storage/java/net/anotheria/portalkit/services/storage/mongo/util/MongoConstants.java
@@ -2,7 +2,7 @@ package net.anotheria.portalkit.services.storage.mongo.util;
 
 /**
  * Mongo constants.
- * 
+ *
  * @author Alexandr Bolbat
  */
 public final class MongoConstants {
@@ -14,7 +14,31 @@ public final class MongoConstants {
 	/**
 	 * No equal operator.
 	 */
-	public static final String NOT_EQUAL_OPERATOR = "$ne";
+	public static final String OPERATOR_NOT_EQUAL = "$ne";
+	/**
+	 * In operator.
+	 */
+	public static final String OPERATOR_IN = "$in";
+	/**
+	 * No in operator.
+	 */
+	public static final String OPERATOR_NOT_IN = "$nin";
+	/**
+	 * Less then operator.
+	 */
+	public static final String OPERATOR_LESS_THAN = "$lt";
+	/**
+	 * Less then or equal operator.
+	 */
+	public static final String OPERATOR_LESS_THAN_OR_EQUAL = "$lte";
+	/**
+	 * Greater then operator.
+	 */
+	public static final String OPERATOR_GREATER_THAN = "$gt";
+	/**
+	 * Greater then or equal operator.
+	 */
+	public static final String OPERATOR_GREATER_THAN_OR_EQUAL = "$gte";
 
 	/**
 	 * Private constructor.
