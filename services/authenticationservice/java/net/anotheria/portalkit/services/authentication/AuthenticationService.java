ee1e6f3e2193e74f0b13d9f42afa34be62a356d5
asamoilich <asamoilich@anotheria.net>
2014-09-29 13:44:39 +0300

    added auth data deletion method

--- services/authenticationservice/java/net/anotheria/portalkit/services/authentication/AuthenticationService.java
+++ services/authenticationservice/java/net/anotheria/portalkit/services/authentication/AuthenticationService.java
@@ -14,49 +14,61 @@ import org.distributeme.annotation.DistributeMe;
 @DistributeMe()
 public interface AuthenticationService extends Service {
 
-	/**
-	 * Sets new password for the user.
-	 * @param id
-	 * @param password
-	 * @throws AuthenticationServiceException
-	 */
-	void setPassword(AccountId id, String password) throws AuthenticationServiceException;
+    /**
+     * Sets new password for the user.
+     *
+     * @param id
+     * @param password
+     * @throws AuthenticationServiceException
+     */
+    void setPassword(AccountId id, String password) throws AuthenticationServiceException;
 
-	/**
-	 * Returns true if the user can authenticate with the given password.
-	 * @param id
-	 * @param password
-	 * @return
-	 * @throws AuthenticationServiceException
-	 */
-	boolean canAuthenticate(AccountId id, String password) throws AuthenticationServiceException;
+    /**
+     * Returns true if the user can authenticate with the given password.
+     *
+     * @param id
+     * @param password
+     * @return
+     * @throws AuthenticationServiceException
+     */
+    boolean canAuthenticate(AccountId id, String password) throws AuthenticationServiceException;
 
-	/**
-	 * Authenticates the user with encrypted token. Returns the id of the authenticated user or throws exception. Note,
-	 * if the token is one-use token, the token is considered used after the call to this method.
-	 * @param token
-	 * @return
-	 * @throws AuthenticationServiceException
-	 */
-	AccountId authenticateByEncryptedToken(String token) throws AuthenticationServiceException;
+    /**
+     * Authenticates the user with encrypted token. Returns the id of the authenticated user or throws exception. Note,
+     * if the token is one-use token, the token is considered used after the call to this method.
+     *
+     * @param token
+     * @return
+     * @throws AuthenticationServiceException
+     */
+    AccountId authenticateByEncryptedToken(String token) throws AuthenticationServiceException;
 
-	/**
-	 * Returns if the user can by authenticate by the token. The difference is that this call doesn't reduce the usability of the token.
-	 * Use for administration perposses.
-	 * @param token
-	 * @return
-	 * @throws AuthenticationServiceException
-	 */
-	boolean canAuthenticateByEncryptedToken(String token) throws AuthenticationServiceException;
+    /**
+     * Returns if the user can by authenticate by the token. The difference is that this call doesn't reduce the usability of the token.
+     * Use for administration perposses.
+     *
+     * @param token
+     * @return
+     * @throws AuthenticationServiceException
+     */
+    boolean canAuthenticateByEncryptedToken(String token) throws AuthenticationServiceException;
 
-	/**
-	 * Creates a new token with same parameters as parameter token. The returned token is already saved in the db (with
-	 * all consequences) and can be issued to the user by calling getEncodedAuthString.
-	 * @param accountId
-	 * @param prefilledToken
-	 * @return
-	 * @throws AuthenticationServiceException
-	 */
-	EncryptedAuthToken generateEncryptedToken(AccountId accountId, AuthToken prefilledToken) throws AuthenticationServiceException;
+    /**
+     * Creates a new token with same parameters as parameter token. The returned token is already saved in the db (with
+     * all consequences) and can be issued to the user by calling getEncodedAuthString.
+     *
+     * @param accountId
+     * @param prefilledToken
+     * @return
+     * @throws AuthenticationServiceException
+     */
+    EncryptedAuthToken generateEncryptedToken(AccountId accountId, AuthToken prefilledToken) throws AuthenticationServiceException;
 
+    /**
+     * Remove auth tokens and password by provided accountId.
+     *
+     * @param accountId provided {@link AccountId}
+     * @throws AuthenticationServiceException on errors
+     */
+    void deleteAuthenticationData(AccountId accountId) throws AuthenticationServiceException;
 }
--- services/authenticationservice/java/net/anotheria/portalkit/services/authentication/AuthenticationServiceImpl.java
+++ services/authenticationservice/java/net/anotheria/portalkit/services/authentication/AuthenticationServiceImpl.java
@@ -18,154 +18,169 @@ import java.util.Set;
  * @since 13.12.12 09:30
  */
 @Monitor(subsystem = "portalkit")
-public class AuthenticationServiceImpl implements AuthenticationService{
+public class AuthenticationServiceImpl implements AuthenticationService {
 
-	/**
-	 * The currently used password enhashing algorithm.
-	 */
-	private PasswordEncryptionAlgorithm passwordAlgorithm;
-	/**
-	 * The persistence service.
-	 */
-	private AuthenticationPersistenceService persistenceService;
+    /**
+     * The currently used password enhashing algorithm.
+     */
+    private PasswordEncryptionAlgorithm passwordAlgorithm;
+    /**
+     * The persistence service.
+     */
+    private AuthenticationPersistenceService persistenceService;
 
-	/**
-	 * Logger.
-	 */
-	private static Logger log = Logger.getLogger(AuthenticationServiceImpl.class);
+    /**
+     * Logger.
+     */
+    private static Logger log = Logger.getLogger(AuthenticationServiceImpl.class);
 
-	/**
-	 * Default constructor.
-	 */
-	public AuthenticationServiceImpl() {
-		AuthenticationServiceConfig config = new AuthenticationServiceConfig();
-		//initialize with configureme.
-		try{
-			passwordAlgorithm = PasswordEncryptionAlgorithm.class.cast(Class.forName(config.getPasswordAlgorithm()).newInstance());
-		}catch(Exception e) {
-			throw new IllegalStateException("Can't operate without configured and available PasswordEncryptionAlgorithm (config="+config.getPasswordAlgorithm()+")", e);
-		}
+    /**
+     * Default constructor.
+     */
+    public AuthenticationServiceImpl() {
+        AuthenticationServiceConfig config = new AuthenticationServiceConfig();
+        //initialize with configureme.
+        try {
+            passwordAlgorithm = PasswordEncryptionAlgorithm.class.cast(Class.forName(config.getPasswordAlgorithm()).newInstance());
+        } catch (Exception e) {
+            throw new IllegalStateException("Can't operate without configured and available PasswordEncryptionAlgorithm (config=" + config.getPasswordAlgorithm() + ")", e);
+        }
 
-		passwordAlgorithm.customize(config.getPasswordKey());
+        passwordAlgorithm.customize(config.getPasswordKey());
 
-		//note, this will work with a) only one AuthenticationPersistenceService impl or b) configured metafactory
-		try{
-			persistenceService = MetaFactory.get(AuthenticationPersistenceService.class);
-		}catch(MetaFactoryException e){
-			throw new IllegalStateException("Can't work without a persistence service", e);
-		}
+        //note, this will work with a) only one AuthenticationPersistenceService impl or b) configured metafactory
+        try {
+            persistenceService = MetaFactory.get(AuthenticationPersistenceService.class);
+        } catch (MetaFactoryException e) {
+            throw new IllegalStateException("Can't work without a persistence service", e);
+        }
 
-	}
+    }
 
-	@Override
-	public void setPassword(AccountId id, String password) throws AuthenticationServiceException {
-		if (id==null)
-			throw new IllegalArgumentException("id can't be null");
-		try{
-			persistenceService.saveEncryptedPassword(id, passwordAlgorithm.encryptPassword(password));
-		}catch(AuthenticationPersistenceServiceException e){
-			throw new AuthenticationServiceException(e);
-		}
-	}
+    @Override
+    public void setPassword(AccountId id, String password) throws AuthenticationServiceException {
+        if (id == null)
+            throw new IllegalArgumentException("id can't be null");
+        try {
+            persistenceService.saveEncryptedPassword(id, passwordAlgorithm.encryptPassword(password));
+        } catch (AuthenticationPersistenceServiceException e) {
+            throw new AuthenticationServiceException(e);
+        }
+    }
 
-	@Override
-	public boolean canAuthenticate(AccountId id, String password) throws AuthenticationServiceException {
-		if (id==null)
-			throw new IllegalArgumentException("id can't be null");
-		String storedEncryptedPassword = null;
-		try{
-			storedEncryptedPassword = persistenceService.getEncryptedPassword(id);
-		}catch(AuthenticationPersistenceServiceException e){
-			throw new AuthenticationServiceException(e);
-		}
-		return storedEncryptedPassword!=null && password != null && storedEncryptedPassword.equals(passwordAlgorithm.encryptPassword(password));
-	}
+    @Override
+    public boolean canAuthenticate(AccountId id, String password) throws AuthenticationServiceException {
+        if (id == null)
+            throw new IllegalArgumentException("id can't be null");
+        String storedEncryptedPassword = null;
+        try {
+            storedEncryptedPassword = persistenceService.getEncryptedPassword(id);
+        } catch (AuthenticationPersistenceServiceException e) {
+            throw new AuthenticationServiceException(e);
+        }
+        return storedEncryptedPassword != null && password != null && storedEncryptedPassword.equals(passwordAlgorithm.encryptPassword(password));
+    }
 
-	@Override
-	public AccountId authenticateByEncryptedToken(String token) throws AuthenticationServiceException {
-		try{
-			boolean tokenExists = persistenceService.authTokenExists(token);
-			if (!tokenExists)
-				throw new AuthTokenNotFoundException();
-		}catch(AuthenticationPersistenceServiceException e){
-			throw new AuthenticationServiceException(e);
-		}
+    @Override
+    public AccountId authenticateByEncryptedToken(String token) throws AuthenticationServiceException {
+        try {
+            boolean tokenExists = persistenceService.authTokenExists(token);
+            if (!tokenExists)
+                throw new AuthTokenNotFoundException();
+        } catch (AuthenticationPersistenceServiceException e) {
+            throw new AuthenticationServiceException(e);
+        }
 
-		AuthToken authToken = decrypt(token);
+        AuthToken authToken = decrypt(token);
 
-		if (authToken.isExpired())
-			throw new AuthTokenExpiredException();
+        if (authToken.isExpired())
+            throw new AuthTokenExpiredException();
 
-		if (!authToken.isMultiUse()){
-			try{
-				persistenceService.deleteAuthToken(authToken.getAccountId(), token);
-			}catch(AuthenticationPersistenceServiceException e){
-				log.warn("Couldn't delete used auth token "+token+" for "+authToken.getAccountId());
-			}
-		}
+        if (!authToken.isMultiUse()) {
+            try {
+                persistenceService.deleteAuthToken(authToken.getAccountId(), token);
+            } catch (AuthenticationPersistenceServiceException e) {
+                log.warn("Couldn't delete used auth token " + token + " for " + authToken.getAccountId());
+            }
+        }
 
-		return authToken.getAccountId();
-	}
+        return authToken.getAccountId();
+    }
 
-	@Override
-	public boolean canAuthenticateByEncryptedToken(String token) throws AuthenticationServiceException {
-		try{
-			boolean tokenExists = persistenceService.authTokenExists(token);
-			if (!tokenExists)
-				return false;
-		}catch(AuthenticationPersistenceServiceException e){
-			throw new AuthenticationServiceException(e);
-		}
+    @Override
+    public boolean canAuthenticateByEncryptedToken(String token) throws AuthenticationServiceException {
+        try {
+            boolean tokenExists = persistenceService.authTokenExists(token);
+            if (!tokenExists)
+                return false;
+        } catch (AuthenticationPersistenceServiceException e) {
+            throw new AuthenticationServiceException(e);
+        }
 
-		AuthToken authToken = decrypt(token);
-		return !authToken.isExpired();
+        AuthToken authToken = decrypt(token);
+        return !authToken.isExpired();
 
-	}
+    }
 
-	/**
-	 * Internal helper abstraction.
-	 * @param token token to decrypt.
-	 * @return decrypted AuthToken.
-	 */
-	private AuthToken decrypt(String token){
-		return AuthTokenEncryptors.decrypt(token);
-	}
-
-
-
-	@Override
-	public EncryptedAuthToken generateEncryptedToken(AccountId accountId, AuthToken prefilledToken) throws AuthenticationServiceException {
-
-		AuthToken newToken = (AuthToken)prefilledToken.clone();
-		String encryption = AuthTokenEncryptors.encrypt(newToken);
-
-		EncryptedAuthToken encToken = new EncryptedAuthToken();
-		encToken.setAuthToken(newToken);
-		encToken.setEncryptedVersion(encryption);
-
-		try{
-			if (newToken.isExclusive())
-				persistenceService.deleteAuthTokens(accountId);
-
-			if (!newToken.isExclusive() && newToken.isExclusiveInType()){
-				//this is maybe suboptimal, but no other chance to fix it otherways for now
-				Set<String> tokens = persistenceService.getAuthTokens(newToken.getAccountId());
-				for (Iterator<String> it = tokens.iterator(); it.hasNext();){
-					String storedToken = it.next();
-					AuthToken t = AuthTokenEncryptors.decrypt(storedToken);
-					if (t.getType()==newToken.getType())
-						persistenceService.deleteAuthToken(newToken.getAccountId(), storedToken);
-				}
-			}
+    /**
+     * Internal helper abstraction.
+     *
+     * @param token token to decrypt.
+     * @return decrypted AuthToken.
+     */
+    private AuthToken decrypt(String token) {
+        return AuthTokenEncryptors.decrypt(token);
+    }
 
 
-			persistenceService.saveAuthToken(accountId, encryption);
-		}catch(AuthenticationPersistenceServiceException e){
-			throw new AuthenticationServiceException(e);
-		}
+    @Override
+    public EncryptedAuthToken generateEncryptedToken(AccountId accountId, AuthToken prefilledToken) throws AuthenticationServiceException {
+
+        AuthToken newToken = (AuthToken) prefilledToken.clone();
+        String encryption = AuthTokenEncryptors.encrypt(newToken);
+
+        EncryptedAuthToken encToken = new EncryptedAuthToken();
+        encToken.setAuthToken(newToken);
+        encToken.setEncryptedVersion(encryption);
+
+        try {
+            if (newToken.isExclusive())
+                persistenceService.deleteAuthTokens(accountId);
+
+            if (!newToken.isExclusive() && newToken.isExclusiveInType()) {
+                //this is maybe suboptimal, but no other chance to fix it otherways for now
+                Set<String> tokens = persistenceService.getAuthTokens(newToken.getAccountId());
+                for (Iterator<String> it = tokens.iterator(); it.hasNext(); ) {
+                    String storedToken = it.next();
+                    AuthToken t = AuthTokenEncryptors.decrypt(storedToken);
+                    if (t.getType() == newToken.getType())
+                        persistenceService.deleteAuthToken(newToken.getAccountId(), storedToken);
+                }
+            }
 
 
+            persistenceService.saveAuthToken(accountId, encryption);
+        } catch (AuthenticationPersistenceServiceException e) {
+            throw new AuthenticationServiceException(e);
+        }
 
-		return encToken;
-	}
+
+        return encToken;
+    }
+
+    @Override
+    public void deleteAuthenticationData(AccountId accountId) throws AuthenticationServiceException {
+        if (accountId == null)
+            throw new IllegalArgumentException("Incoming accountId is NULL.");
+        try {
+            persistenceService.deleteAuthTokens(accountId);
+        } catch (AuthenticationPersistenceServiceException e) {
+            log.error("Couldn't delete auth tokens for " + accountId);
+        }
+        try {
+            persistenceService.deleteEncryptedPassword(accountId);
+        } catch (AuthenticationPersistenceServiceException e) {
+            log.error("Couldn't delete encrypt password for " + accountId);
+        }
+    }
 }
