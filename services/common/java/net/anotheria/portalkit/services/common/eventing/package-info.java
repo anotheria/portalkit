23b26e96c460d3bdcd0e33eba7600acf7bea358d
Leon Rosenberg <leon.rosenberg@anotheria.net>
2015-12-30 17:32:15 +0100

    migrated to std maven layout

--- services/common/java/net/anotheria/portalkit/services/common/eventing/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Classes related to service eventing.
- */
-package net.anotheria.portalkit.services.common.eventing;
--- services/common/java/net/anotheria/portalkit/services/common/scheduledqueue/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The implementation of a queue that is processing elements in the planning schedule.
- * 
- * @author Alexandr Bolbat
- */
-package net.anotheria.portalkit.services.common.scheduledqueue;
