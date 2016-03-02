e0956ab56c63f5dc42e6ad2b62cc018ac581b0d2
BodyaVan <bodya.van@gmail.com>
2016-03-02 12:59:38 +0200

    MAT-2410 - Create general JpaSpringConfiguration for all services

---
+++ services/common/src/main/java/net/anotheria/portalkit/services/common/spring/JpaSpringConfiguration.java
@@ -0,0 +1,99 @@
+package net.anotheria.portalkit.services.common.spring;
+
+import net.anotheria.portalkit.services.common.persistence.jdbc.JDBCConfig;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.configureme.ConfigurationManager;
+import org.hibernate.dialect.PostgreSQL82Dialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * @author bvanchuhov
+ */
+@Configuration
+@EnableTransactionManagement
+public class JpaSpringConfiguration {
+
+    public String getJdbcConfigurationName() {
+        throw new UnsupportedOperationException("No implementation for getJdbcConfigurationName()");
+    }
+
+    public String getEntityPackagesToScan() {
+        throw new UnsupportedOperationException("No implementation for getEntityPackagesToScan()");
+
+    }
+
+    public JDBCConfig getJdbcConfig() {
+        JDBCConfig jdbcConfig = new JDBCConfig();
+        ConfigurationManager.INSTANCE.configureAs(jdbcConfig, getJdbcConfigurationName());
+        return jdbcConfig;
+    }
+
+    @Bean(destroyMethod="close")
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+
+        JDBCConfig jdbcConfig = getJdbcConfig();
+        dataSource.setDriverClassName(jdbcConfig.getDriver());
+        dataSource.setUrl(jdbcConfig.getUrl());
+        dataSource.setUsername(jdbcConfig.getUsername());
+        dataSource.setPassword(jdbcConfig.getPassword());
+        dataSource.setValidationQuery("SELECT 1");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.POSTGRESQL);
+        jpaVendorAdapter.setShowSql(true);
+        jpaVendorAdapter.setGenerateDdl(false);
+        jpaVendorAdapter.setDatabasePlatform(PostgreSQL82Dialect.class.getName());
+
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+//    @DependsOn("flyway")
+    public EntityManagerFactory entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactoryBean.setPackagesToScan(getEntityPackagesToScan());
+
+        Properties props = new Properties();
+        props.put("hibernate.show_sql", true);
+        props.put("hibernate.hbm2ddl.auto", "validate");
+        entityManagerFactoryBean.setJpaProperties(props);
+
+        entityManagerFactoryBean.afterPropertiesSet();
+
+        return entityManagerFactoryBean.getObject();
+    }
+
+//    @Bean(initMethod = "migrate")
+//    public Flyway flyway(DataSource dataSource) {
+//        Flyway flyway = new Flyway();
+//        flyway.setDataSource(dataSource);
+//        return flyway;
+//    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
