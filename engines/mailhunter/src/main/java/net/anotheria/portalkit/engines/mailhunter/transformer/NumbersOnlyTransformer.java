d34632b9736e8f4cdef118fecc604abfe3df0eb2
Vlad Lukjanenko <lukjanenko99@gmail.com>
2016-08-01 10:30:58 +0300

    NumbersOnlyTransformer: only 0-9 numbers

--- engines/mailhunter/src/main/java/net/anotheria/portalkit/engines/mailhunter/transformer/NumbersOnlyTransformer.java
+++ engines/mailhunter/src/main/java/net/anotheria/portalkit/engines/mailhunter/transformer/NumbersOnlyTransformer.java
@@ -9,7 +9,7 @@ public class NumbersOnlyTransformer extends AbstractTransformer {
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (Character.isDigit(c))
+            if (c >= '0' && c <= '9')
                 ret.append(c);
         }
         return ret.toString();
