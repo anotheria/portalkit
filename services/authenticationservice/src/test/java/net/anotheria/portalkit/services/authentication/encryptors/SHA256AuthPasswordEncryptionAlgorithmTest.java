2af41d169645aa108eacaa0961777f3eb40d28e0
Leon Rosenberg <leon.rosenberg@anotheria.net>
2018-01-19 23:20:33 +0100

    fixed unittest

--- services/authenticationservice/src/test/java/net/anotheria/portalkit/services/authentication/encryptors/SHA256AuthPasswordEncryptionAlgorithmTest.java
+++ services/authenticationservice/src/test/java/net/anotheria/portalkit/services/authentication/encryptors/SHA256AuthPasswordEncryptionAlgorithmTest.java
@@ -14,16 +14,22 @@ import static org.junit.Assert.assertFalse;
 public class SHA256AuthPasswordEncryptionAlgorithmTest {
 	@Test
 	public void testDifferentPhrases(){
-		String enc1 = new SHA256PasswordEncryptionAlgorithm().encryptPassword("password1");
-		String enc2 = new SHA256PasswordEncryptionAlgorithm().encryptPassword("password2");
+		String enc1 = SHA256PasswordEncryptionAlgorithm().encryptPassword("password1");
+		String enc2 = SHA256PasswordEncryptionAlgorithm().encryptPassword("password2");
 		assertFalse(enc1.equals(enc2));
 	}
 	@Test
 	public void testSamePhrase(){
-		String enc1 = new SHA256PasswordEncryptionAlgorithm().encryptPassword("password1");
-		String enc2 = new SHA256PasswordEncryptionAlgorithm().encryptPassword("password1");
-		String enc3 = new SHA256PasswordEncryptionAlgorithm().encryptPassword("pass"+"word"+1);
+		String enc1 = SHA256PasswordEncryptionAlgorithm().encryptPassword("password1");
+		String enc2 = SHA256PasswordEncryptionAlgorithm().encryptPassword("password1");
+		String enc3 = SHA256PasswordEncryptionAlgorithm().encryptPassword("pass"+"word"+1);
 		assertEquals(enc1, enc2);
 		assertEquals(enc1, enc3);
+	}
+
+	private  SHA256PasswordEncryptionAlgorithm SHA256PasswordEncryptionAlgorithm(){
+		SHA256PasswordEncryptionAlgorithm alg = new SHA256PasswordEncryptionAlgorithm();
+		alg.customize("MYKEY");
+		return alg;
 	}
 }
