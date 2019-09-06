70b11910c4300c0a20738ccaebf8dfb6c1c998af
Leon Rosenberg <leon.rosenberg@anotheria.net>
2019-09-06 09:13:56 +0200

    added annotations

--- services/authenticationservice/src/main/java/net/anotheria/portalkit/services/authentication/SecretKeyAuthenticationService.java
+++ services/authenticationservice/src/main/java/net/anotheria/portalkit/services/authentication/SecretKeyAuthenticationService.java
@@ -1,10 +1,16 @@
 package net.anotheria.portalkit.services.authentication;
 
+import org.distributeme.annotation.DistributeMe;
+import org.distributeme.annotation.FailBy;
+import org.distributeme.core.failing.RetryCallOnce;
+
 /**
  * TODO comment this class
  *
  * @author lrosenberg
  * @since 2019-09-06 08:57
  */
-public interface SecretKeyAuthenticationService {
+@DistributeMe()
+@FailBy(strategyClass= RetryCallOnce.class)
+public interface SecretKeyAuthenticationService extends AuthenticationService {
 }
