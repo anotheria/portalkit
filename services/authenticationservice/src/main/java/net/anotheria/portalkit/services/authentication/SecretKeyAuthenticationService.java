439d7669a559b711a25b18d45a4249f9041b0621
Leon Rosenberg <leon.rosenberg@anotheria.net>
2019-09-06 08:59:14 +0200

    added interface for secretkey

---
+++ services/authenticationservice/src/main/java/net/anotheria/portalkit/services/authentication/SecretKeyAuthenticationService.java
@@ -0,0 +1,10 @@
+package net.anotheria.portalkit.services.authentication;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 2019-09-06 08:57
+ */
+public interface SecretKeyAuthenticationService {
+}
