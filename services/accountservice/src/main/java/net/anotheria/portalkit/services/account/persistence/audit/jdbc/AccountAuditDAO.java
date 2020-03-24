cacc8c41e9b2c02b33ee408c0426d26ba3b84c00
Leon Rosenberg <leon.rosenberg@anotheria.net>
2020-03-24 18:36:04 +0100

    checkstyle

--- services/accountservice/src/main/java/net/anotheria/portalkit/services/account/persistence/audit/jdbc/AccountAuditDAO.java
+++ services/accountservice/src/main/java/net/anotheria/portalkit/services/account/persistence/audit/jdbc/AccountAuditDAO.java
@@ -10,7 +10,10 @@ import net.anotheria.portalkit.services.common.persistence.jdbc.JDBCUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
