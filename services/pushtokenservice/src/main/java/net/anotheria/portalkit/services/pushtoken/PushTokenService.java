8ec14b80dc7ccd8d6d1d1144157bf4a42b9c9ca7
uniloftsky <uniloftsky@gmail.com>
2023-03-21 19:39:17 +0100

    push-token-service - added implementation for PushTokenService. Added in-memory implementation for PushTokenPersistenceService

---
+++ services/pushtokenservice/src/main/java/net/anotheria/portalkit/services/pushtoken/PushTokenService.java
@@ -0,0 +1,45 @@
+package net.anotheria.portalkit.services.pushtoken;
+
+import net.anotheria.anoprise.metafactory.Service;
+import net.anotheria.portalkit.services.common.AccountId;
+import org.distributeme.annotation.DistributeMe;
+import org.distributeme.annotation.FailBy;
+import org.distributeme.core.failing.RetryCallOnce;
+
+import java.util.List;
+
+/**
+ * PushToken service is used to store and manage data about push-notification tokens.
+ * One user (accountId) can have many push-notification tokens.
+ * Every token is unique. So two users cannot have the same tokens.
+ */
+@DistributeMe
+@FailBy(strategyClass = RetryCallOnce.class)
+public interface PushTokenService extends Service {
+
+    /**
+     * Returns a list of tokens that are belonged to provided user.
+     *
+     * @param accountId user whom push-notification tokens want to retrieve
+     * @return list of tokens
+     */
+    List<String> getAllByAccountId(AccountId accountId) throws PushTokenServiceException;
+
+    /**
+     * Adds a provided token to the list of user's tokens
+     *
+     * @param accountId account that will have a new or one more token
+     * @param token     token to add
+     * @return added token
+     */
+    String addTokenForAccount(AccountId accountId, String token) throws PushTokenServiceException;
+
+    /**
+     * Deletes a token. After deletion, no user will have this token.
+     *
+     * @param token token to delete
+     * @return returns account that was owner of token
+     */
+    AccountId removeToken(String token) throws PushTokenServiceException;
+
+}
---
+++ services/pushtokenservice/src/main/java/net/anotheria/portalkit/services/pushtoken/PushTokenServiceImpl.java
@@ -0,0 +1,98 @@
+package net.anotheria.portalkit.services.pushtoken;
+
+import net.anotheria.anoprise.cache.Cache;
+import net.anotheria.anoprise.cache.Caches;
+import net.anotheria.anoprise.metafactory.MetaFactory;
+import net.anotheria.anoprise.metafactory.MetaFactoryException;
+import net.anotheria.portalkit.services.common.AccountId;
+import net.anotheria.portalkit.services.pushtoken.persistence.PushTokenPersistenceService;
+import net.anotheria.portalkit.services.pushtoken.persistence.PushTokenPersistenceServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class PushTokenServiceImpl implements PushTokenService {
+
+    /**
+     * Logger.
+     */
+    private static Logger log = LoggerFactory.getLogger(PushTokenServiceImpl.class);
+
+    /**
+     * Persistence service.
+     */
+    private PushTokenPersistenceService persistenceService;
+
+    /**
+     * Cache by {@link AccountId}.
+     */
+    private Cache<AccountId, List<String>> cache;
+
+    /**
+     * Default Constructor.
+     */
+    public PushTokenServiceImpl() {
+        cache = Caches.createHardwiredCache("pushtokenservice-cache");
+        try {
+            persistenceService = MetaFactory.get(PushTokenPersistenceService.class);
+        } catch (MetaFactoryException e) {
+            throw new IllegalStateException("Can't start without persistence service ", e);
+        }
+    }
+
+    @Override
+    public List<String> getAllByAccountId(AccountId accountId) throws PushTokenServiceException {
+        try {
+            List<String> cached = cache.get(accountId);
+            if (cached != null) {
+                return cached;
+            }
+
+            List<String> fromPersistence = persistenceService.getAllTokensByAccountId(accountId);
+            cache.put(accountId, fromPersistence);
+            return fromPersistence;
+        } catch (PushTokenPersistenceServiceException ex) {
+            log.error("Cannot get tokens by account", ex);
+            throw new PushTokenServiceException("Cannot get tokens by account", ex);
+        }
+    }
+
+    @Override
+    public String addTokenForAccount(AccountId accountId, String token) throws PushTokenServiceException {
+        try {
+            removeToken(token); // returns an account who had this token before
+            String savedToken = persistenceService.saveTokenForAccount(accountId, token);
+
+            // cache sync
+            List<String> cached = cache.get(accountId);
+            if (cached != null) {
+                cached.add(token);
+                cache.put(accountId, cached);
+            }
+            return savedToken;
+        } catch (PushTokenPersistenceServiceException ex) {
+            log.error("Cannot add token for account", ex);
+            throw new PushTokenServiceException("Cannot add token for account", ex);
+        }
+    }
+
+    @Override
+    public AccountId removeToken(String token) throws PushTokenServiceException {
+        try {
+            AccountId tokenOwner = persistenceService.deleteToken(token);
+
+            // cache sync
+            List<String> cached = cache.get(tokenOwner);
+            if (cached != null) {
+                cached.remove(token);
+                cache.put(tokenOwner, cached);
+            }
+
+            return tokenOwner;
+        } catch (PushTokenPersistenceServiceException ex) {
+            log.error("Cannot remove token", ex);
+            throw new PushTokenServiceException("Cannot remove token", ex);
+        }
+    }
+}
