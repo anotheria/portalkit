5bd4829fe3f21bc7fa04bd01ee518f8cf53ca431
uniloftsky <uniloftsky@gmail.com>
2023-07-21 13:40:48 +0200

    admin-rest-api - Documented AccountResource

--- admin-api/src/main/java/net/anotheria/portalkit/adminapi/rest/account/AccountResource.java
+++ admin-api/src/main/java/net/anotheria/portalkit/adminapi/rest/account/AccountResource.java
@@ -2,13 +2,12 @@ package net.anotheria.portalkit.adminapi.rest.account;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
 import net.anotheria.anoplass.api.APIException;
-import net.anotheria.anoplass.api.APIFactory;
 import net.anotheria.anoplass.api.APIFinder;
 import net.anotheria.anosite.cms.user.CMSUserManager;
 import net.anotheria.portalkit.adminapi.api.admin.*;
@@ -95,8 +94,13 @@ public class AccountResource {
     }
 
     @GET
+    @Operation(description = "Returns account info by accountId")
     @Path("/{accountId}")
-    public Response getAccountById(@PathParam("accountId") String accountId) {
+    @ApiResponse(
+            description = "Account object.",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminAccountAO.class)))
+    public Response getAccountById(@PathParam("accountId") @Parameter(name = "accountId", description = "Account to get") String accountId) {
         AdminAccountAO result = null;
         try {
             result = adminAPI.getAccountById(new AccountId(accountId));
@@ -108,6 +112,16 @@ public class AccountResource {
     }
 
     @POST
+    @Operation(description = "Update account information. Payload accepts email, tenant, brand, name, type.",
+            requestBody = @RequestBody(description = "Payload to update account object information.<br>" +
+                    "Only ID field is required, other fields are not-required<br>" +
+                    "Only a not-null field will be updated in the target account object.<br>" +
+                    "For example if only 'email' field is present in payload, then only email will be updated. Another fields leave as before.",
+                    content = @Content(schema = @Schema(implementation = AccountUpdateRequest.class))))
+    @ApiResponse(
+            description = "Updated account object.",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminAccountAO.class)))
     public Response updateAccount(AccountUpdateRequest request) {
         AdminAccountAO result = null;
 
@@ -125,8 +139,13 @@ public class AccountResource {
     }
 
     @POST
+    @Operation(description = "Adds account status to given account.")
     @Path("add-status/{accountId}/{status}")
-    public Response addStatusToAccount(@PathParam("accountId") String accountId, @PathParam("status") int status) {
+    @ApiResponse(
+            description = "Updated account object.",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminAccountAO.class)))
+    public Response addStatusToAccount(@PathParam("accountId") @Parameter(description = "Account to add new status") String accountId, @PathParam("status") @Parameter(description = "Status to add") int status) {
         AdminAccountAO result = null;
         try {
             result = adminAPI.addAccountStatus(new AccountId(accountId), status);
@@ -137,8 +156,13 @@ public class AccountResource {
     }
 
     @POST
+    @Operation(description = "Removes account status from given account.")
     @Path("remove-status/{accountId}/{status}")
-    public Response removeStatusFromAccount(@PathParam("accountId") String accountId, @PathParam("status") int status) {
+    @ApiResponse(
+            description = "Updated account object.",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AdminAccountAO.class)))
+    public Response removeStatusFromAccount(@PathParam("accountId") @Parameter(description = "Account from where remove a status") String accountId, @PathParam("status") @Parameter(description = "Status to remove") int status) {
         AdminAccountAO result = null;
         try {
             result = adminAPI.removeAccountStatus(new AccountId(accountId), status);
@@ -149,6 +173,8 @@ public class AccountResource {
     }
 
     @POST
+    @Operation(description = "Changes account password.", requestBody = @RequestBody(description = "Payload to change account password.",
+            content = @Content(schema = @Schema(implementation = AccountSetPasswordRequest.class))))
     @Path("password")
     public Response setNewPassword(AccountSetPasswordRequest request) {
         try {
@@ -160,8 +186,13 @@ public class AccountResource {
     }
 
     @GET
+    @Operation(description = "Get auth token to login as provided account.")
     @Path("sign-as/{accountId}")
-    public Response getSignAsToken(@PathParam("accountId") String accountId) {
+    @ApiResponse(
+            description = "Auth token.",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = String.class)))
+    public Response getSignAsToken(@PathParam("accountId") @Parameter(description = "account whose token will be created to perform log in") String accountId) {
         String result = null;
         try {
             result = adminAPI.getSignAsToken(new AccountId(accountId));
