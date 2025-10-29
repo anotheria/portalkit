b61d2fbf60f86c218302a105aa29cdd5b6db0e14
Leon Rosenberg <rosenberg.leon@gmail.com>
2025-10-29 15:54:25 +0100

    adjusted configuration, AccountService is not an ENUM anymore

--- services/accountservice-spring-mongo/src/main/java/net/anotheria/portalkit/services/account/AccountBeans.java
+++ services/accountservice-spring-mongo/src/main/java/net/anotheria/portalkit/services/account/AccountBeans.java
@@ -10,9 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class AccountBeans {
     @Bean
     public AccountServiceImpl accountServiceImpl(AccountEntityRepository accountEntityRepository, AccountNoteEntityRepository accountNoteEntityRepository, AccountAuditEntityRepository accountAuditEntityRepository) {
-        AccountServiceImpl.INSTANCE.setAccountEntityRepository(accountEntityRepository);
-        AccountServiceImpl.INSTANCE.setAccountNoteEntityRepository(accountNoteEntityRepository);
-        AccountServiceImpl.INSTANCE.setAccountAuditEntityRepository(accountAuditEntityRepository);
-        return AccountServiceImpl.INSTANCE;
+        return new AccountServiceImpl(accountEntityRepository, accountNoteEntityRepository, accountAuditEntityRepository);
     }
 }
